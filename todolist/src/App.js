d5f8f6155d1b9384f937b76912b828b93f56fa49
JoRan29 <jokavana29@gmail.com>
2020-04-11 15:26:35 +0200

    props

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import ToDoItem from "./ToDoItem";
+import Joke from "./Joke";
 
 function App() {
   return (
     <div className="todoList">
       <ToDoItem />
       <ToDoItem />
+      <Joke question="Is this a joke?" punchline="Yes!" />
       <ToDoItem />
       <ToDoItem />
     </div>
