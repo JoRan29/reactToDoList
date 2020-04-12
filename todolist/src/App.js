0a2a4208fcec9a5d9504fe22a187b575b5370ed7
JoRan29 <jokavana29@gmail.com>
2020-04-12 16:29:07 +0200

    switched app to class component, not funct

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -7,23 +7,44 @@ import Product from "./Product";
 import productsData from "./productsData";
 import todoData from "./todoData";
 
-function App() {
-  const jokeComponents = jokesData.map((j) => {
-    return <Joke key={j.id} question={j.question} punchline={j.punchline} />;
-  });
-  const productsComponents = productsData.map((p) => {
-    return <Product key={p.id} type={p.type} price={p.price} />;
-  });
-  const todoComponent = todoData.map((t) => {
-    return <ToDoItem key={t.id} text={t.text} completed={t.completed} />;
-  });
-  return (
-    <div className="todoList">
-      {todoComponent}
-      {jokeComponents}
-      {productsComponents}
-    </div>
-  );
+// function App() {
+//   const jokeComponents = jokesData.map((j) => {
+//     return <Joke key={j.id} question={j.question} punchline={j.punchline} />;
+//   });
+//   const productsComponents = productsData.map((p) => {
+//     return <Product key={p.id} type={p.type} price={p.price} />;
+//   });
+//   const todoComponent = todoData.map((t) => {
+//     return <ToDoItem key={t.id} text={t.text} completed={t.completed} />;
+//   });
+//   return (
+//     <div className="todoList">
+//       {todoComponent}
+//       {jokeComponents}
+//       {productsComponents}
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    const jokeComponents = jokesData.map((j) => {
+      return <Joke key={j.id} question={j.question} punchline={j.punchline} />;
+    });
+    const productsComponents = productsData.map((p) => {
+      return <Product key={p.id} type={p.type} price={p.price} />;
+    });
+    const todoComponent = todoData.map((t) => {
+      return <ToDoItem key={t.id} text={t.text} completed={t.completed} />;
+    });
+    return (
+      <div className="todoList">
+        {todoComponent}
+        {jokeComponents}
+        {productsComponents}
+      </div>
+    );
+  }
 }
 
 export default App;
